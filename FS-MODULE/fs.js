c43da9269858e0885e791d2cf3951b23d749374b
dheeftech <dheeftech@gmail.com>
2025-09-15 10:45:26 +0300

    delete file

--- FS-MODULE/fs.js
+++ FS-MODULE/fs.js
@@ -18,10 +18,14 @@ if (!fs.existsSync(folderPath)) {
 //append more content
 // fs.appendFileSync(filePath, "This is an appended line of text. \n");
 
-fs.appendFileSync(
-  filePath,
-  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpos"
-);
+// fs.appendFileSync(
+//   filePath,
+//   "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpos"
+// );
 
-const content = fs.readFileSync(filePath, "utf-8");
-console.log(content);
+// const content = fs.readFileSync(filePath, "utf-8");
+// console.log(content);
+
+//deleting file
+fs.unlinkSync(filePath);
+console.log("file deleted");
